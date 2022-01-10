b9fd174897a5e52f91d68914f127f60f121115ff
Varis Rana <vrvarisrana123@gmail.com>
2022-01-10 09:35:27 +0530

    commit 10

--- Amazon/DeleteNNodesAfterMNodesOfLinkedList.java
+++ Amazon/DeleteNNodesAfterMNodesOfLinkedList.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+// Que-->> https://practice.geeksforgeeks.org/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/1/
 public class DeleteNNodesAfterMNodesOfLinkedList {
 
 }
