03418f5ac28d401dbb734f11132cf6e881abb8c4
Varis Rana <vrvarisrana123@gmail.com>
2022-01-10 09:36:52 +0530

    commit 10

--- Amazon/DeleteNNodesAfterMNodesOfLinkedList.java
+++ Amazon/DeleteNNodesAfterMNodesOfLinkedList.java
@@ -1,5 +1,35 @@
 import java.util.*;
-// Que-->> https://practice.geeksforgeeks.org/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/1/
-public class DeleteNNodesAfterMNodesOfLinkedList {
 
+// Que-->> https://practice.geeksforgeeks.org/problems/delete-n-nodes-after-m-nodes-of-a-linked-list/1/
+class Node {
+      int data;
+      Node next;
+
+      Node(int data) {
+            this.data = data;
+            this.next = null;
+      }
+}
+
+public class DeleteNNodesAfterMNodesOfLinkedList {
+      public static void linkdelete(Node head, int M, int N) {
+            Node temp = head;
+            int mul = 1, count = 1;
+            int x = N;
+
+            while (temp != null && temp.next != null) {
+                  if (count == M * mul) {
+                        Node prev = temp;
+                        while (x > 0 && temp.next != null) {
+                              temp = temp.next;
+                              x--;
+                        }
+                        prev.next = temp.next;
+                        mul++;
+                        x = N;
+                  }
+                  count++;
+                  temp = temp.next;
+            }
+      }
 }
